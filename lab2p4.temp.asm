cef62d78fde0fc7188e8d7d7a05b8c288120715d
MuhammadUsmanBash <muhammadusman48014@gmail.com>
2024-03-25 21:14:34 +0500

    Initial commit

---
+++ lab2p4.temp.asm
@@ -0,0 +1,17 @@
+TITLE Divide -10 by 3
+INCLUDE c:\Users\Muhammad Usman\.vscode\extensions\istareatscreens.masm-runner-0.4.5\native\irvine\Irvine32.inc
+.data
+
+.code
+main PROC
+
+    mov eax, 0h
+    mov ebx, 0h
+    mov eax, dividend/divisor   ;This Moves the value of -10/3 in the EAX
+    mov ebx, (dividend MOD divisor)    ;This Moves the value of remainder of -10/3 in EBX
+    
+    ;TO Display the registers
+    call DumpRegs
+exit
+main ENDP
+END main
