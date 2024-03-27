ee2fbd36ae105683ce8c61e6c1a6262428b115dd
MuhammadUsmanBash <muhammadusman48014@gmail.com>
2024-03-27 22:40:29 +0500

    New Tasks

---
+++ Lab 3/Task1.temp.asm
@@ -0,0 +1,17 @@
+TITLE Evaluated statement using: “5+(6-2)” using two register
+INCLUDE c:\Users\Muhammad Usman\.vscode\extensions\istareatscreens.masm-runner-0.4.5\native\irvine\Irvine32.inc
+.data
+
+mbyte BYTE 05,12,100
+Align 2
+mword WORD 50h, 60h
+mdouble DWORD 0A0908070h
+greetings BYTE "Hi There",0
+Response TEXTEQU <'Hi, Thanks.'>
+Reply BYTE Response
+.code
+ main PROC
+
+  exit
+main ENDP
+END main
